e4a6681f86215edc24c49bf427355848cdfb8b68
frontendgod <3054750147@qq.com>
2020-02-17 20:54:54 +0800

    安装echart兼容所需要的依赖

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -49,7 +49,12 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [
+          resolve('src'),
+          resolve('test'),
+          resolve('node_modules/vue-echarts'),
+          resolve('node_modules/resize-detector')
+        ]
       },
       {
         test: /\.svg$/,
--- src/components/form-design/Cell.vue
+++ src/components/form-design/Cell.vue
@@ -113,6 +113,11 @@
         :style="{'text-align': data.options.align, 'font-size': data.options.fontSize}"
         @click="activeCell"
       >{{data.value}}</p>
+
+      <!-- <div class="echarts" v-if="data.type === 'chart'">
+        <v-chart :options="polar"/>
+      </div> -->
+
       <FDGridPanel
         v-else
         :propData="data"
@@ -177,13 +182,19 @@
 </template>
 
 <script>
+import ECharts from 'vue-echarts'
+import 'echarts/lib/chart/bar'
+import 'echarts/lib/chart/line'
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/polar'
 import common from '@/utils/common'
 import FDGridPanel from '@/components/form-design/FDGridPanel'
 import bus from '@/utils/bus'
 
 export default {
   components: {
-    FDGridPanel
+    FDGridPanel,
+    'v-charts': ECharts
   },
   props: {
     formAttr: {
@@ -346,7 +357,48 @@ export default {
     }
   },
   data () {
+    let data = []
+
+    for (let i = 0; i <= 360; i++) {
+      let t = i / 180 * Math.PI
+      let r = Math.sin(2 * t) * Math.cos(2 * t)
+      data.push([r, i])
+    }
     return {
+      polar: {
+        title: {
+          text: '极坐标双数值轴'
+        },
+        legend: {
+          data: ['line']
+        },
+        polar: {
+          center: ['50%', '54%']
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross'
+          }
+        },
+        angleAxis: {
+          type: 'value',
+          startAngle: 0
+        },
+        radiusAxis: {
+          min: 0
+        },
+        series: [
+          {
+            coordinateSystem: 'polar',
+            name: 'line',
+            type: 'line',
+            showSymbol: false,
+            data: data
+          }
+        ],
+        animationDuration: 2000
+      },
       ideaDialogVisible: false,
       ideaTabIndex: '1'
     }
@@ -355,6 +407,10 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.echarts {
+  width: 100%;
+  height: 100%;
+}
 .cell {
   background-color: #eee;
   padding: 10px 10px 20px 10px;
